ddd295a15da77f9d1e6ed3f92b7c899896d08590
zhufeip <141732041+zhufeip@users.noreply.github.com>
2023-08-10 21:30:31 +0800

    Create test1.c
    switch分支语句的使用

---
+++ test1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	 int month;
+	
+	scanf_s("%d", &month);
+	switch (month)//switch(整形)
+	{
+		case 1:
+			printf("%d月\n", month);
+			break;
+		case 2:
+			printf("%d月\n", month);
+			break;
+		case 3:
+			printf("%d月\n", month);
+			break;
+		case 4:
+			printf("%d月\n", month);
+			break;
+		case 5:
+			printf("%d月\n", month);
+	}
+	return 0;
+}
